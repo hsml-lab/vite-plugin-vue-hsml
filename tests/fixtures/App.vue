83a81638c38867a18c8661330528bfcf4fbc779f
Shinigami92 <chrissi92@hotmail.de>
2026-03-22 21:15:51 +0100

    infra: add Vite build integration test and ts-check script

---
+++ tests/fixtures/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0);
+</script>
+
+<template lang="hsml">
+div#app
+  h1.text-xl.font-bold Hello HSML
+  p The count is {{ count }}
+  button(@click="count++") Increment
+  ul
+    li(v-for="i in 3" :key="i") Item {{ i }}
+</template>
